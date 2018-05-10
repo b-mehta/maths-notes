2cb77f19d58fce3225a3dd89c476a02a71622cec
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-05-10 06:31:06 +0100

    helper programs

---
+++ process.py
@@ -0,0 +1,15 @@
+import sys
+import re
+
+lines = []
+input_file = sys.argv[1]
+first = int(sys.argv[2])
+with open(input_file, 'r') as filename:
+    for line in filename:
+        lines.append(line)
+
+y = re.compile(r"\\hypertarget\{def:(\w*)\}")
+
+for (num,line) in enumerate(lines, 1):
+    for i in y.findall(line):
+        print(num, i, end='\n')
