2f76103f927c28c78bb46eaaebc6ff7418a3ac6c
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-10-04 03:34:20 +0100

    better parser

--- scripts/parse.hs
+++ scripts/parse.hs
@@ -3,6 +3,8 @@ import System.Environment
 import Text.LaTeX.Base
 import Text.LaTeX.Base.Parser
 import Text.LaTeX.Base.Syntax
+import Control.Monad
+import Data.List
 
 thm,thp,def :: [String]
 thm = ["nthm","ncor","nlemma","nprop","thm","cor","lemma","prop"]
@@ -38,10 +40,33 @@ fix :: [String] -> LaTeX -> LaTeX
 fix envs doc = getPreamble doc <> document (modify envs body)
   where Just body = getBody doc
 
+dropR :: Int -> [a] -> [a]
+dropR n l = go (drop n l) l
+  where
+    go [] _ = []
+    go (_:xs) (y:ys) = y:go xs ys
+
+takeR :: Int -> [a] -> [a]
+takeR n l = go (drop n l) l
+  where
+    go [] r = r
+    go (_:xs) (_:ys) = go xs ys
+
+runFile :: String -> IO ()
+runFile loc =
+  if ".tex" `isSuffixOf` loc
+     then do
+            when (take 3 (takeR 7 loc) `notElem` ["thm", "thp", "def"]) $ do
+            when (takeR 10 loc /= "header.tex") $ do
+            parsed <- parseLaTeXFile loc
+            case parsed of
+              Left _ -> putStrLn ("Failed to parse " ++ loc ++ ", skipping.")
+              Right y -> zipWithM_ func ["thm", "thp", "def"] [thm, thp, def]
+                where loc' = dropR 4 loc
+                      func a b = renderFile (loc' ++ "_" ++ a ++ ".tex") (fix b y)
+     else putStrLn (loc ++ " is an invalid filename.")
+
 main :: IO ()
 main = do
-  [loc] <- getArgs
-  Right y <- parseLaTeXFile (loc ++ ".tex")
-  renderFile (loc ++ "_thp.tex") (fix thp y)
-  renderFile (loc ++ "_thm.tex") (fix thm y)
-  renderFile (loc ++ "_def.tex") (fix def y)
+  files <- getArgs
+  forM_ files runFile
