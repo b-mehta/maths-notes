b90c50b1e6a0e51c400f0c64342c021ad99a426c
Bhavik Mehta <bhavikmehta8@gmail.com>
2019-01-20 16:00:44 +0000

    scripting edits

--- scripts/process.py
+++ scripts/process.py
@@ -3,7 +3,10 @@ import re
 
 lines = []
 input_file = sys.argv[1]
-first = int(sys.argv[2])
+try:
+    first = int(sys.argv[2])
+except:
+    first = 0
 with open(input_file, 'r') as filename:
     for line in filename:
         lines.append(line)
@@ -12,4 +15,5 @@ y = re.compile(r"\\hypertarget\{(\w*:\w*)\}")
 
 for (num,line) in enumerate(lines, 1):
     for i in y.findall(line):
-        print(num, i, end='\n')
+        if num > first:
+            print(num, i, end='\n')
--- scripts/warning.hs
+++ scripts/warning.hs
@@ -5,7 +5,15 @@ import Control.Monad
 go :: String -> [String]
 go = tail . filter (isInfixOf "warning") . lines
 
+run :: String -> IO ()
+run log = case go log of
+            [] -> putStrLn "No warnings!"
+            x -> mapM_ putStrLn x
+
+main :: IO ()
 main = do
   [file] <- getArgs
   log <- readFile (file ++ ".log")
-  mapM_ putStrLn (go log)
+  case log of
+    [] -> putStrLn "compiling tex file..."
+    _ -> run log
